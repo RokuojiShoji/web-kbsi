04248cc1793bd62209a52fefcd618ab19262f0de
RokuojiShoji <fahmi.kems15@gmail.com>
2021-10-07 15:28:42 +0700

    add test content homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -33,65 +33,37 @@ function HomePage() {
     height: "100vh",
   };
 
+  const im = {
+    position: 'sticky',
+    top: '10rem',
+    color: '#cacaca',
+  }
+
+  const spacer = {
+    height: '40%',
+  }
+
+  const ident = {
+    color: '#cacaca',
+    fontSize: '2rem',
+    textAlign: 'justify',
+  }
   return (
     <>
-      <Row className="intro bg-img g-0"></Row>
-      <Row className="g-0 justify-content-center">
-        <Carousel className="w-50 p-4 px-3">
-          <Carousel.Item>
-            <img className="d-block w-100" src={test} alt="First slide" />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="d-block w-100" src={test} alt="Second slide" />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="d-block w-100" src={test} alt="Third slide" />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-        <Carousel className="w-50 p-4 px-3">
-          <Carousel.Item>
-            <img className="d-block w-100" src={test} alt="First slide" />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="d-block w-100" src={test} alt="Second slide" />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="d-block w-100" src={test} alt="Third slide" />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      </Row>
+      <Container fluid ="true" className="intro bg-img g-0 align-items-center">
+        <div style={spacer}></div>
+        <Col  style={im} className="text-center start">
+          <h1>Siapa Kami ?</h1>
+        </Col>
+      </Container>
+      <div style={{height:'2rem'}}></div>
+      <Container className="w-75">
+        <p style={ident}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+          nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </Container>
     </>
   );
 }
