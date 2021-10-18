a73f214ea41708f85e28d1406e0847cbee7a7eb5
RokuojiShoji <fahmi.kems15@gmail.com>
2021-10-18 11:03:30 +0700

    homepage done, visimisi done

--- src/components/Navi.jsx
+++ src/components/Navi.jsx
@@ -49,13 +49,12 @@ function Navi() {
                     Laporan
                   </NavDropdown.Item>
                 </NavDropdown>
-                <NavDropdown title="Produk" id="drop-product">
+                <Nav.Link href="/produk">Produk</Nav.Link>
+                {/* <NavDropdown title="Produk" id="drop-product">
                   <NavDropdown.Item href="/simpanan">Simpanan</NavDropdown.Item>
-                  <NavDropdown.Item href="/pembiayaan">
-                    Pembiayaan
-                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/pembiayaan">Pembiayaan</NavDropdown.Item>
                   <NavDropdown.Item href="/ziswaf">Ziswaf</NavDropdown.Item>
-                </NavDropdown>
+                </NavDropdown> */}
                 <NavDropdown title="Anggota" id="drop-member">
                   <NavDropdown.Item href="/pendaftaranAnggota">
                     Pendaftaran Anggota
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,8 @@
-
 import {Col, Row, Container} from "react-bootstrap";
 import { useEffect } from "react";
-
+import '../image/wallet.svg'
+import svgWallet from '../components/svgWallet'
+import wallet from '../image/logoBsi.png'
 
 function HomePage() {
   useEffect(() => {
@@ -41,7 +42,7 @@ function HomePage() {
 
   const ident = {
     color: '#cacaca',
-    fontSize: '2rem',
+    fontSize: '1.7rem',
     textAlign: 'justify',
   }
 
@@ -53,23 +54,31 @@ function HomePage() {
   }
 
   const produkCard ={
-    height:'9rem',
+    height:'20rem',
+    width:'20rem',
     borderRadius: '0.5rem',
   }
   const visi = {
-    fontSize: '2rem',
+    fontSize: '1.7rem',
     color: '#4d4d4d',
     textAlign: 'justify'
   }
+
+  const link = {
+    color: '#616161 !important',
+    textAlign: 'right',
+  }
+
   return (
     <>
+      {/* intro */}
       <Container fluid="true" className="intro bg-img g-0 align-items-center">
         <div style={spacer}></div>
         <Col  style={im} className="text-center start">
           <h1 className='rubik-550'style={{fontSize:'3.5rem'}}>Siapa Kami ?</h1>
         </Col>
       </Container>
-      <Container style={{width:'65vw'}} className="my-5">
+      <Container style={{width:'60vw'}} className="my-5">
         <p style={ident}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
           quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
@@ -77,6 +86,7 @@ function HomePage() {
         </p>
       </Container>
       
+      {/* visi */}
       <Container fluid='true' className="g-0 py-5 bg-gw">
         <Row className="justify-content-center g-0">
           <Col className="my-auto" lg="3">
@@ -84,19 +94,31 @@ function HomePage() {
               Visi Kami
             </h1>
           </Col>
-          <Col lg="7" >
-            <p style={visi} className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <Col lg="6" >
+            <p style={visi} className="">
+            Terwujudnya Ekonomi Islam sebagai nilai bersama demi kemajuan dan kesinambungan koperasi dan 
+            meningkatkan kesejahteraan lahir dan bathin masyarakat Indonesia menuju kebaikan di dunia dan di akhirat 
+            </p>
+            <div style={link}>
+              <a className='rubik link' href='/visiMisi' >lihat selengkapnya --></a>
+            </div>
           </Col>
         </Row>
       </Container>
-      {/* <div style={{height:'3rem'}}></div> */}
 
+      {/* produk */}
       <Container fluid='true' className="justify-content-center py-5 bg-rb">
-        <Col className='mx-auto' style={{width:'60vw'}}>
-          <h1 className='rubik-550' style={produk}>Produk Kami</h1>
-          <div style={produkCard} className='bg-gw my-4'></div>
-          <div style={produkCard} className='bg-gw my-4'></div>
-        </Col>
+      <h1 className='rubik-bold' style={produk}>Produk Kami</h1>
+        <Row className='mx-auto' style={{width:'80vw'}}>
+          <Col>
+            <div style={produkCard} className='bg-gw my-5 mx-auto'>
+              <img src={wallet}></img>
+            </div>
+          </Col>
+          <Col >
+            <div style={produkCard} className='bg-gw my-5 mx-auto'></div>
+          </Col>
+        </Row>
       </Container>
 
       <div style={{height:'2px'}} className='bg-gw'></div>
