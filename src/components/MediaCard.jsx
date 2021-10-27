a85855ffeea1082826dd3b26a6580c8e51c5748b
RokuojiShoji <fahmi.kems15@gmail.com>
2021-10-27 10:57:31 +0700

    media page component done

--- src/components/MediaCard.jsx
+++ src/components/MediaCard.jsx
@@ -1,45 +1,90 @@
 import React from "react";
 import { Carousel, CarouselItem, Col, Container, Row } from "react-bootstrap";
-import mat191 from '../image/MAT TB 2019/mat-19-1.jpg'
-import mat192 from '../image/MAT TB 2019/mat-19-2.jpg'
+import mat191 from "../image/MAT TB 2019/mat-19-1.jpg";
+import mat192 from "../image/MAT TB 2019/mat-19-2.jpg";
 
 function ImgSlide(props) {
-	
-	const imgStyle = {
-		display: 'block',
-		width: '100%',
-	}
+  const imgStyle = {
+    display: "block",
+    width: "100%",
+    padding: "1rem",
+  };
 
-	return (
-    <Carousel>
-      {props.src1 != null &&
-				<CarouselItem>
-        	<img src={props.src1} style={imgStyle}/>
-      	</CarouselItem>
-			}
-			{props.src2 != null &&
-				<CarouselItem>
-        	<img src={props.src2} />
-      	</CarouselItem>
-			}
-			{props.src3 != null &&
-				<CarouselItem>
-        	<img src={props.src3} />
-      	</CarouselItem>
-			}
+  return (
+    <Carousel nextIcon="" prevIcon="">
+      {props.src1 != null && (
+        <CarouselItem>
+          <img src={props.src1} style={imgStyle} />
+        </CarouselItem>
+      )}
+      {props.src2 != null && (
+        <CarouselItem>
+          <img src={props.src2} style={imgStyle}/>
+        </CarouselItem>
+      )}
+      {props.src3 != null && (
+        <CarouselItem>
+          <img src={props.src3} style={imgStyle}/>
+        </CarouselItem>
+      )}
     </Carousel>
   );
 }
 
 export default function MediaCard(props) {
-	return (
-    <Row>
-      <Col>
-        <ImgSlide src1={mat191} src2={props.url2} src3={props.url3} />
+  const text = {
+    textAlign: "center",
+    margin: "auto",
+  };
+  const line = {
+    borderLeft: "2px solid #696969",
+    borderRight: "2px solid #696969",
+    height: "45%",
+    width: "1px",
+    marginLeft: "auto",
+    marginRight: "auto",
+  };
+  return (
+    <Row className="justify-content-center">
+      {props.side == 'left' && (
+      <>		
+			<Col xl="5">
+        <ImgSlide src1={props.url1} src2={props.url2} src3={props.url3} />
       </Col>
-			<Col>
-				<p>deskripsi foto</p>
-			</Col>
+      <Col xl="1" className="g-0 justify-content-center">
+        <Row style={line} />
+        <Row style={{ height: "10%" }}>
+          <p style={{ color: "#696969", margin: "auto", textAlign: "center" }}>
+            {props.year}
+          </p>
+        </Row>
+        <Row style={line} />
+      </Col>
+      <Col xl="5" className="justify-content-center d-flex">
+        <h2 style={text}>{props.text}</h2>
+      </Col>
+			</>
+			)}
+
+{props.side == 'right' && (
+      <>
+			<Col xl="5" className="justify-content-center d-flex">
+				<h2 style={text}>{props.text}</h2>
+      </Col>
+      <Col xl="1" className="g-0 justify-content-center">
+        <Row style={line} />
+        <Row style={{ height: "10%" }}>
+          <p style={{ color: "#696969", margin: "auto", textAlign: "center" }}>
+            {props.year}
+          </p>
+        </Row>
+        <Row style={line} />
+      </Col>
+      <Col xl="5" >
+				<ImgSlide src1={props.url1} src2={props.url2} src3={props.url3} />
+      </Col>
+			</>
+			)}
     </Row>
   );
 }
--- src/pages/Media.js
+++ src/pages/Media.js
@@ -1,15 +1,29 @@
 import { Container, Col, Row } from "react-bootstrap";
 import { Carousel  } from "react-bootstrap";
 import MediaCard from "../components/MediaCard";
+import rat2010 from '../image/RAT TB 2010/rat2010.jpg'
+import rat20131 from '../image/RAT TB 2013/rat20131.jpg'
+import rat20132 from '../image/RAT TB 2013/rat20132.jpg'
 
 
 function Media () {
 	return (
 		<Container>
 			<Col>
-				<Row>
-					 <MediaCard />
-				</Row>
+				<div style={{height:'10px'}}></div>
+				<MediaCard 
+					side='left'
+					year='2011'
+					text='Deskripsi RAT TB 2010'
+					url1={rat2010}
+				/>
+				<MediaCard 
+					side='right'
+					year='2014'
+					text='Deskripsi RAT TB 2013'
+					url1={rat20131}
+					url2={rat20132}
+				/>
 			</Col>
 		</Container>
 	)
