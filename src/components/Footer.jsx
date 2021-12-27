e932f39f8cfadc77053d4dbaadfeba983aad4537
Fahmi Izzuddin <fahmi.kems15@gmail.com>
2021-12-27 05:41:39 +0000

    rework footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
 import { Col, Container, Row } from "react-bootstrap";
 import "../App.css"
 import logo from "../image/logoBsi.png"
+import {FaInstagram, FaWhatsapp, FaTelegram} from 'react-icons/fa'
+import {MdLocationOn} from 'react-icons/md'
+import Separator from "./Separator";
 export default function Footer () {
 	
+  const mediaIcon = {
+    color: '#F0EFF4',
+    outlineColor: '#F0EFF4',
+    width: '40px',
+    height: '40px'
+  }
   const alamat = {
     textAlign: "left",
     color: "#F0EFF4"
@@ -10,24 +19,19 @@ export default function Footer () {
   return (
     <Container fluid="true">
       <Col className="footer bg-rb">
-        <Row className="g-0 footer-top">
-          <Col lg={3} className="px-4">
-            <img className="d-block w-100"src={logo} alt="logo"/>
-          </Col>
-          <Col className="justify-content-start" lg={2}>
-            <p style={alamat}>
-              <h2 className="h rubik-bold">Alamat</h2>
-              <p className="rubik-light">
-                Komp. Permata Kopo Blok <br/> H-62 Desa Sayati, Kec. Margahayu, Kab. Bandung 40228
-              </p>
-            </p>
-          </Col>
-          <Col className="px-4" lg={7}>
-
-          </Col>
+        <Separator height='1.5rem'/>
+        <Row className="g-0 footer-logo">
+          <img src={logo} className='py-1' style={{height:'90%', width:'auto'}}/>
         </Row>
+        <Row className="g-0 footer-media w-25 mx-auto">
+          <a style={{width:'auto'}} href='https://www.instagram.com/baiturrahimsyariah'><FaInstagram style={mediaIcon}/></a>
+          <a style={{width:'auto'}} href='https://wa.me/+6285104711175'><FaWhatsapp style={mediaIcon}/></a>
+          <a style={{width:'auto'}} href='https://t.me/Kopbsi'><FaTelegram style={mediaIcon}/></a>
+          <a style={{width:'auto'}} href='https://goo.gl/maps/UwyJN33t56aMbeve6'><MdLocationOn style={mediaIcon}/></a>
+        </Row>
+        <Row className="g-0 px-2 text-center justify-content-center footer-bottom bg-rb rubik">Copyright &copy; Koperasi bsi </Row>
       </Col>
-      <div className="px-2 text-end footer-bottom bg-rb rubik">Copyright &copy; Koperasi bsi </div>
+      
     </Container>
   );
 }
