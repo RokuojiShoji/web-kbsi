cac1e655104c2f908a5a6235effc3ae748dc0b59
Fahmi Izzuddin <fahmi.kems15@gmail.com>
2022-01-10 06:28:40 +0000

    add confirmation

--- src/pages/Anggota/PendaftaranAnggota.js
+++ src/pages/Anggota/PendaftaranAnggota.js
@@ -1,13 +1,16 @@
-import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import { Container, Row, Col, Form, Button, FormText } from 'react-bootstrap'
 import emailjs, { init } from '@emailjs/browser'
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
+import Separator from '../../components/Separator'
+import {Redirect} from 'react-router-dom'
 
 init('user_COW0bZlVe8lhcgD9ObueN')
 
 function FormMaster(props) {
 	const label = {
 		color: '#cacaca',
-		fontSize: '1rem'
+		fontSize: '1rem',
+		marginBottom: '0px'
 	}
 
 	const formStyle = {
@@ -18,23 +21,50 @@ function FormMaster(props) {
 		<Form.Group className="mb-2">
 			<Form.Label style={label}>{props.label}</Form.Label>
 			<Form.Control style={formStyle} type={props.type} name={props.name} placeholder={props.placeholder} />
+			<FormText>{props.sub}</FormText>
 		</Form.Group>
+	)
+}
+
+function Confirmation() {
+	const style = {
+		width:'80%',
+		color:'#cacaca',
+		fontSize:'1rem',
+		borderRadius:'0.3rem',
+		textAlign:'center',
+		padding:'1rem',
+		margin:'1rem auto 1.5rem auto'
+	}
+
+	return(
+		<Container style={style} className='bg-pp'>
+			Data anda sudah terkirim!
+		</Container>
 	)
 }
 
 function PendaftaranAnggota() {
 
 	const form = useRef();
-
+  const [show, setShow] = useState(false)
 	const sendEmail = (e) => {
 		e.preventDefault();
 
 		emailjs.sendForm('service_37nf1ne', 'template_ucqpikd', form.current,)
 			.then((result) => {
 				console.log(result.text);
+				setShow(true);
+				window.scrollTo({
+					top: 0,
+					left: 0,
+					behavior: 'smooth'
+				})
 			}, (error) => {
 				console.log(error.text);
 			});
+
+		
 	};
 
 	const contain = {
@@ -52,32 +82,49 @@ function PendaftaranAnggota() {
 		<Col>
 			<h1 style={header} className='rubik-bold'>Form Pendaftaran Anggota</h1>
 			<Container style={contain} className='rubik-light w-50 bg-rb mt-2 mb-2 py-3'>
+				{
+				show?<Confirmation />
+				:null
+				} 
 				<Form ref={form} onSubmit={sendEmail}>
 					<FormMaster 
 						label='Nama Lengkap' 
 						type='text' 
-						name='namaLengkap' 
-						//placeholder='masukkan nama lengkap' 
+						name='namaLengkap'
 					/>
 					<FormMaster 
 						label='Nama Panggilan' 
 						type='text' 
 						name='namaPanggilan' 
-						//placeholder='masukkan nama panggilan' 
 					/>
 					<FormMaster 
 						label='Tempat / Tanggal Lahir' 
 						type='text' 
 						name='TTL' 
-						placeholder='kota / hh-bb-tttt' 
+						placeholder='kota / hh-bb-tttt'
+						sub='Format = kota / hh-bb-tttt' 
 					/>
+					<FormMaster
+						label='Pekerjaan'
+						type='text'
+						name='pekerjaan'
+					/>
+					<FormMaster
+						label='Alamat'
+						type='textarea'
+						name='alamat'
+						sub='masukkan alamat sesuai KTP'
+					/>	
 
 					{/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
 						<Form.Check style={label} type="checkbox" label="Check me out" />
 					</Form.Group> */}
-					<Button variant="primary" type="submit">
-						Submit
-					</Button>
+					<Separator height='2rem'/>
+					<div className='w-100'>
+						<Button className='' variant="primary" type="submit">
+							Submit
+						</Button>
+					</div>
 				</Form>
 			</Container>
 		</Col>
