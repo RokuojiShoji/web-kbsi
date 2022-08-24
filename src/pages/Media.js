d22f64b40691cbb956fa255250135d1b38cffa5a
Fahmi Izzuddin <fahmi.kems15@gmail.com>
2022-08-24 06:56:13 +0000

    revert media

--- src/pages/Media.js
+++ src/pages/Media.js
@@ -1,7 +1,6 @@
-import React from "react";
-import {Container, Col, Row, Accordion, Modal} from "react-bootstrap";
-// import AlbumModal from "../components/AlbumModal";
-import { Test } from "../components/Test";
+import {Container, Col} from "react-bootstrap";
+import MediaCard from "../components/MediaCard";
+import Separator from "../components/Separator";
 
 import rat20101 from '../image/RAT TB 2010/rat2010.jpg'
 import rattb121 from '../image/RAT TB 2012/rattb121.jpg'
@@ -25,14 +24,91 @@ import tim2018 from '../image/Tim BRS/2018.jpg'
 import tim2019 from '../image/Tim BRS/2019.jpg'
 import tim2021 from '../image/Tim BRS/2021.jpg'
 
-
-
 function Media () {
-	return(
-		<>
-			{/* <AlbumModal /> */}
-			<Test />
-		</>
+	return (
+		<Container>
+			<Col>
+				<Separator height='1rem' />
+				<MediaCard 
+					side='left'
+					year='2011'
+					text='RAT TB 2010'
+					url1={rat20101}
+				/>
+				<MediaCard 
+					side='right'
+					year='2013'
+					text='RAT TB 2012'
+					url1={rattb121}
+					url2={rattb122}
+					url3={rattb123}
+				/>
+				<MediaCard 
+					side='left'
+					year='2014'
+					text='RAT TB 2013'
+					url1={rat20131}
+					url2={rat20132}
+				/>
+				<MediaCard
+					side='right'
+					year='2016'
+					text='Launching Bank Sampah Baiturrahim'
+					url1={bankSampah}
+				/>
+				<MediaCard 
+					side='left'
+					text='MAT TB 2016'
+					url1={mat20161}
+				/>
+				<MediaCard
+					side='right'
+					year='2017'
+					text='Kantor Lama bsi'
+					url1={kantorLama}
+				/>
+				<MediaCard
+					side='left'
+					year='2019'
+					text='Rihlah ke The Jungle'
+					url1={rihlahJungle}
+				/>
+				<MediaCard
+					side='right'
+					year='2020'
+					text='Rihlah ke Pangandaran'
+					url1={rihlahPangandaran}
+				/>
+				<MediaCard
+					side='left'
+					text='MAT TB 2019'
+					url1={mat20191}
+					url2={mat20192}
+				/>
+				<MediaCard
+					side='right'
+					year='2021'
+					text='Kantor Baru bsi'
+					url1={kantorBaru}
+				/>
+				<Separator height='1rem' />
+				<MediaCard
+					side='left'
+					text='Bazaar'
+					url1={bazaar1}
+				/>
+				<MediaCard
+					side='right'
+					text='Tim Baiturrahim Syariah'
+					url1={tim2015}
+					url2={tim2016}
+					url3={tim2018}
+					url4={tim2019}
+					url5={tim2021}
+				/>
+				<Separator height='1rem' />
+			</Col>
+		</Container>
 	)
 }
 
